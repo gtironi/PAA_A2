166256f61ac43ea1d61b4c1ce9503f64895f6005
lfamarciano <lfamarciano@gmail.com>
2024-12-04 20:31:19 -0300

    caminho de taxi finalizado

--- temp_tarefa_3.cpp
+++ temp_tarefa_3.cpp
@@ -27,7 +27,8 @@ void melhorRota(GraphAdjList& graph, int origem, int destino, int custoMax) {
     // float tempoMetro = rotaMetro(graph, rota, custoMax);
     // float tempoOnibus = rotaOnibus(graph, rota, custoMax);
     // float tempoMetroOnibus = rotaMetroOnibus(graph, rota, custoMax);
-    cout << "foi" << endl;
+    cout << tempoTaxi << endl;
+    cout << "===================================" << endl;
     return;
 }
 
@@ -41,51 +42,60 @@ float rotaTaxi(GraphAdjList& graphCompleto, GraphAdjList& graphDirecionado, Node
     // combino os dois vetores parent para achar a rota
     
     // Distância máxima que da para andar de taxi com o custo
-    int distMax = (int)(custoMax/TAXA_TAXI);
-
-    // Inicializando vetores
-    int distanceTaxi[graphDirecionado.numVertices()];
-    int parentTaxi[graphDirecionado.numVertices()];
-    
-    // Dijkstra para achar o menor caminho de taxi até o endereço
-    Dijkstra dijkstra;
-    dijkstra.compute(graphDirecionado, origem, parentTaxi, distanceTaxi);
-
-
-    int dist = 0;
+    int distMax = (int)(custoMax/10);//taxa taxi
     int vertice = destino;
-    int filho;
-    IntList caminhoTaxi;
-    // Loop para achar o vértice mais longe que da pra ir de taxi
-    while (vertice != parentTaxi[vertice]){
+    Dijkstra dijkstra;
+    float tempoTaxi =0;
+    if (distMax >=1){ // se dist_max<1 quer dizer que o usuário não tem dinheiro para andar de taxi, pois o valor mínimo de uma corrida é igual a TAXA
+        // Inicializando vetores
+        int distanceTaxi[graphDirecionado.numVertices()];
+        int parentTaxi[graphDirecionado.numVertices()];
+        // Dijkstra para achar o menor caminho de taxi até o endereço
+        dijkstra.compute(graphDirecionado, origem, parentTaxi, distanceTaxi);
+        
+        int dist = 0;
+        int finalRua;
+        IntList caminhoTaxi;
+        // Loop para achar o vértice mais longe que da pra ir de taxi
+        while (vertice != parentTaxi[vertice]){
+            caminhoTaxi.insert_front(vertice);
+            dist = distanceTaxi[vertice];
+            if (dist < distMax){ break;} // no momento que acha um vértice que da para chegar de taxi quebra
+            finalRua = vertice;
+            vertice = parentTaxi[vertice];
+        }
         caminhoTaxi.insert_front(vertice);
-        dist = distanceTaxi[vertice];
-        if (dist < distMax){ break;}
-        filho = vertice;
-        vertice = parentTaxi[vertice];
+
+        
+        
+        // Se conseguir chegar no destino só de taxi acaba
+        if (vertice == destino){
+            float tempo = calculaTempoTaxi(graphDirecionado, origem, destino, 0, 0, parentTaxi);
+            rota.append(1, tempo, &caminhoTaxi);
+            return tempo;
+        }
+
+        // Calcula distancia restante que da para andar na dentro da rua seguinte ao último vértice que deu pra chegar de taxi
+        // Vamos andar essa distância de taxi
+        int distRestante = distMax - dist;
+        // Cria vértice no lugar em que ele parou de taxi na rua, dessa forma podemos fazer dijkstra a partir dele
+        graphCompleto.splitEdge(vertice, finalRua, distRestante);
+
+        
+        // Calcula tempo da viagem de taxi
+        if (vertice != origem){
+            tempoTaxi  = calculaTempoTaxi(graphDirecionado, origem, vertice, finalRua, distRestante, parentTaxi);
+            rota.append(1, tempoTaxi, &caminhoTaxi); // adicionamos o caminho do taxi na rota
+        }
     }
-    caminhoTaxi.insert_front(vertice);
-
-    // Se conseguir chegar no destino só de taxi retona
-    if (vertice == destino){
-        float tempo = calculaTempoTaxi(graphDirecionado, origem, destino, 0, 0, parentTaxi);
-        rota.append(1, tempo, &caminhoTaxi);
-        return tempo;
-    }
-    int distRestante = distMax - dist;
-
-    graphCompleto.splitEdge(vertice, filho, distRestante);
-
-    float tempoTaxi  = calculaTempoTaxi(graphDirecionado, origem, vertice, filho, distRestante, parentTaxi);
-    rota.append(1, tempoTaxi, &caminhoTaxi);
-
+    // Dijkstra do vértice final do caminho do taxi até o dentino final
     int parentCaminhada[graphCompleto.numVertices()];
     int distanceCaminhada[graphCompleto.numVertices()];
     dijkstra.compute(graphCompleto, graphCompleto.numVertices()-1, parentCaminhada, distanceCaminhada);
 
     vertice = destino;
     IntList caminhoCaminhada;
-    // Loop para achar o vértice mais longe que da pra ir de taxi
+    // Loop para achar o caminho que será feito a pé
     while (vertice != parentCaminhada[vertice]){
         caminhoCaminhada.insert_front(vertice);
         vertice = parentCaminhada[vertice];
@@ -98,62 +108,92 @@ float rotaTaxi(GraphAdjList& graphCompleto, GraphAdjList& graphDirecionado, Node
     return tempoTaxi + tempoCaminhada;
 }
 
-float calculaTempoTaxi(GraphAdjList& graph, int origem, int destino, int filho, int distRestante, int* parentTaxi){
-    float tempo;
-
+float calculaTempoTaxi(GraphAdjList& graph, int origem, int destino, int filho, int distRestante, int* parentTaxi) {
+    float tempo = 0.0f;
+    int v_max;
+    int cost;
+    float modf_transito = 0.9; // Modificador do trânsito (fixo por enquanto)
     int vertice = destino;
-    while (vertice != parentTaxi[vertice]){
+
+    while (vertice != parentTaxi[vertice]) {
+
+        // Busca a aresta conectando parentTaxi[vertice] a vertice
         EdgeNode* edge = graph.getEdges(parentTaxi[vertice]);
+        bool encontrou = false;
+
         while (edge) {
             int v2 = edge->otherVertex();
-            if (v2==vertice) {
-                int cost = edge->cost();
-                int v_max = edge->maxSpeed();
-                float modf_transito = get_transito(); // usa a api de tansito para buscar o modificar da velocidade maxima da via
-                tempo = tempo + cost/(v_max*modf_transito);
+            if (v2 == vertice) {
+                cost = edge->cost();
+                v_max = edge->maxSpeed();
+                tempo += cost / (v_max * modf_transito);
+                encontrou = true;
                 break;
             }
-        vertice = parentTaxi[vertice];
+            edge = edge->next(); // Próxima aresta
         }
+
+        if (!encontrou) {
+            cerr << "Erro: Não foi encontrada uma aresta entre " << parentTaxi[vertice] 
+                 << " e " << vertice << endl;
+            break;
+        }
+
+        // Atualiza o vértice para o pai
+        vertice = parentTaxi[vertice];
     }
 
-    if (distRestante!=0) {
+    // Caso haja uma distância restante a ser percorrida
+    if (distRestante != 0) {
         EdgeNode* edge = graph.getEdges(destino);
         while (edge) {
             int v2 = edge->otherVertex();
-            if (v2==filho) {
+            if (v2 == filho) {
                 v_max = edge->maxSpeed();
-                modf_transito = get_transito(); // usa a api de tansito para buscar o modificar da velocidade maxima da via
+                tempo += distRestante / (v_max * modf_transito);
                 break;
             }
-        vertice = parentTaxi[vertice];
+            edge = edge->next();
         }
-        tempo = tempo + distRestante/(v_max*modf_transito);
     }
 
     return tempo;
 }
 
 
-float calculaTempoCaminhada(GraphAdjList& graph, int origem, int destino, int* parentCaminhada){
-    float tempo;
-
+float calculaTempoCaminhada(GraphAdjList& graph, int origem, int destino, int* parentCaminhada) {
+    float tempo = 0.0f;
     int vertice = destino;
-    while (vertice != parentCaminhada[vertice]){
+
+    // Percorrendo o caminho de destino até a origem utilizando parentCaminhada
+    while (vertice != parentCaminhada[vertice]) {
         EdgeNode* edge = graph.getEdges(parentCaminhada[vertice]);
+        bool encontrou = false;
+
+        // Procura pela aresta que conecta parentCaminhada[vertice] a vertice
         while (edge) {
             int v2 = edge->otherVertex();
-            if (v2==vertice) {
+            if (v2 == vertice) {
                 int cost = edge->cost();
                 int v_max = edge->maxSpeed();
-                float modf_transito = get_transito(); // usa a api de tansito para buscar o modificar da velocidade maxima da via
-                tempo = tempo + cost/(v_max*modf_transito);
+                float modf_transito = 0.9; // Modificador de trânsito (fixo por enquanto)
+                tempo += cost / (v_max * modf_transito);
+                encontrou = true;
                 break;
             }
-        vertice = parentTaxi[vertice];
+            edge = edge->next(); // Próxima aresta
         }
-    }
-    return tempo
-}
 
+        if (!encontrou) {
+            cerr << "Erro: Não foi encontrada uma aresta entre " << parentCaminhada[vertice] 
+                 << " e " << vertice << endl;
+            break;
+        }
+
+        // Atualiza o vértice para o pai
+        vertice = parentCaminhada[vertice];
+    }
+
+    return tempo;
+}
 
