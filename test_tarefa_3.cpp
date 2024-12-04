3c1ae246ce2e567bca663d93dd9c9693bc8fcdfe
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2024-12-04 16:45:43 -0300

    cria arquivo de teste para tarefa 3

--- test_tarefa_3.cpp
+++ test_tarefa_3.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cassert>
+
 #include "graph.h"
-#include "dijkstra.h"
-#include "prim.h"
 
 int main() {
     // Grafo de táxi pra teste
-    // Criar grafo de teste
-    GraphAdjList graphTaxi(6);
+    GraphAdjList graphTaxi(13);
 
     int stdSizeEdge = 100;
     int stdMaxSpeed = 60;
@@ -16,7 +14,6 @@ int main() {
     // Adiciona arestas
     graphTaxi.addEdge(0, 1, "Centro", stdSizeEdge, stdMaxSpeed, !oneWay);
     graphTaxi.addEdge(0, 2, "Centro", stdSizeEdge, stdMaxSpeed, !oneWay);
-    graphTaxi.addEdge(1, 3, "Centro", stdSizeEdge, stdMaxSpeed, !oneWay);
     graphTaxi.addEdge(1, 3, "Centro", stdSizeEdge, stdMaxSpeed, !oneWay);
     graphTaxi.addEdge(3, 4, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
     graphTaxi.addEdge(3, 5, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
@@ -31,6 +28,27 @@ int main() {
     graphTaxi.addEdge(9, 10, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
     graphTaxi.addEdge(10, 12, "Centro", stdSizeEdge, stdMaxSpeed, !oneWay);
     graphTaxi.addEdge(11, 10, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+
+    // Grafo de metro pra teste
+    GraphAdjList graphMetro(13);
+
+    // Copiando aresta dos vértices 4 a 9
+    graphMetro.addEdge(3, 4, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(3, 5, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(3, 7, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(4, 6, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(5, 7, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(6, 8, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(6, 10, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(7, 8, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(7, 9, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(8, 10, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+    graphMetro.addEdge(9, 10, "Centro", stdSizeEdge, stdMaxSpeed, oneWay);
+
+    GraphAdjList* graphMetroBidirectional = graphMetro.createBidirectionalCopy();
+
+    cout << "Printando grafo de metro: " << endl;
+    graphMetroBidirectional->print();   
 
     return 0;
 }
