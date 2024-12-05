f7ab8b97165b01175f7e3ce067d21f6d1204d5cc
ddanieldma <danielalmemiranda@gmail.com>
2024-12-04 21:23:17 -0300

    função que retorna estação mais próxima

--- metro.h
+++ metro.h
@@ -24,5 +24,8 @@ IntList* createPathMetro(GraphAdjList& graphMetro, int start, int end);
 
 float timeMetro(GraphAdjList& graphMetro, IntList& path, const int SPEED_METRO);
 
+bool vertexInGraphMetro(vertex v, GraphAdjList& graphMetro);
+
+vertex closestMetroStation(vertex start, GraphAdjList& graphStreets, GraphAdjList& graphMetro);
 
 #endif // METRO_H
