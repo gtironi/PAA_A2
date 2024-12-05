5ba676a324c485d16d1a2bd4c697f0bb7e030fac
ddanieldma <danielalmemiranda@gmail.com>
2024-12-05 00:28:51 -0300

    adiciona arquivos de utils

---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "list.h"
+
+// ======================================================================
+// Funções de caminho
+IntList* pathVertices(int start, int end, int* parent);
+
+#endif // UTILS_H
