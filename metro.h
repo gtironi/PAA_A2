0d0f1869c822ab2ce852a2dc7f82872fb3eb52f9
ddanieldma <danielalmemiranda@gmail.com>
2024-12-05 00:29:42 -0300

    retira funções que não são de metro

--- metro.h
+++ metro.h
@@ -2,24 +2,15 @@
 #define METRO_H
 
 #include <iostream>
-#include <string>
 
 #include "graph.h"
 #include "dijkstra.h"
 #include "list.h"
+#include "utils.h"
 
 using namespace std;
 
-// ======================================================================
-// Funções de caminho
-IntList* pathVertices(int start, int end, int* parent);
 
-// ======================================================================
-// Funções auxiliares
-void printVector(const vector<int>& vector);
-
-// ======================================================================
-// Funções de metro
 IntList* createPathMetro(GraphAdjList& graphMetro, int start, int end);
 
 float timeMetro(GraphAdjList& graphMetro, IntList& path, const int SPEED_METRO);
@@ -27,5 +18,6 @@ float timeMetro(GraphAdjList& graphMetro, IntList& path, const int SPEED_METRO);
 bool vertexInGraphMetro(vertex v, GraphAdjList& graphMetro);
 
 vertex closestMetroStation(vertex start, GraphAdjList& graphStreets, GraphAdjList& graphMetro);
+
 
 #endif // METRO_H
