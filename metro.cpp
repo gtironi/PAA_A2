11fbfea4b053ba2e176192fccd4e45e5214b0629
ddanieldma <danielalmemiranda@gmail.com>
2024-12-05 00:29:42 -0300

    retira funções que não são de metro

--- metro.cpp
+++ metro.cpp
@@ -1,39 +1,5 @@
 #include "metro.h"
 
-// ======================================================================
-// Funções de caminho
-
-// Recebe vértices de origem, destino, vetor de país gerado por uma função
-// de caminho e o comprimento do caminho e retorna caminho no formato de 
-// lista
-IntList* pathVertices(int start, int end, int* parent) {
-    // Lista para armazenar o caminho
-    IntList * path = new IntList;
-
-    // Começamos no destinho
-    vertex currentVertex = end;
-
-    // E vamos voltando
-    while (currentVertex != start)
-    {
-        path->insert_front(currentVertex);
-        currentVertex = parent[currentVertex];
-    }
-
-    // Primeira posição do caminho é a origem
-    path->insert_front(currentVertex);
-
-    return path;
-}
-
-
-// ======================================================================
-// Funções auxiliares
-void printVector(const vector<int>& vector){
-    for (int i = 0; i < vector.size(); i++){
-        cout << vector[i] << " ";
-    }
-}
 
 // ======================================================================
 // Funções de metro
